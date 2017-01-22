ec2952193263bca50ecd2a11db665e9e828f15aa
perrottg <greg@perrott.net.nz>
2017-01-22 14:05:18 +1300

    Fixes #31
    Added Spansih translation.

---
+++ Locales/enES.lua
@@ -0,0 +1,31 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("EasyRider", "enES")
+
+if not L then
+	return
+end
+
+L["Alt+Left-Click"] = "Alt+click izquierdo"
+L["Auto Hide"] = "Ocultar automáticamente"
+L["Ctrl+Left-Click"] = "Ctrl+click izquierdo"
+L["Horizontal"] = "Horizontal"
+L["If the preferred mount has not been set then a favorite or random mount will be summoned instead."] = "Si no se ha elegido una montura, se invocará una aleatoria o una favorita."
+L["Left"] = "Izquierda"
+L["Open options  menu"] = "Abrir menú de opciones"
+L["Orientation"] = "Orientación"
+L["Preferred Mount"] = "Montura preferida"
+L["Right"] = "Derecha"
+L["Right-Click"] = "Click derecho"
+L["Set current mount as preferred"] = "Asignar montura actual como preferida"
+L["Summon a favorite mount"] = "Invocar montura favorita"
+L["Summon Aquatic Mount"] = "Invocar montura acuática"
+L["Summon Flying Mount"] = "Invocar montura voladora"
+L["Summon Ground Mount"] = "Invocar montura terrestre"
+L["Summon random mount"] = "Invocar montura aleatoria"
+L["Summon Vendor Mount"] = "Invocar montura con vendedor"
+L["Summons and  dismisses the preferred aquatic mount."] = "Invoca o retira la montura acuática preferida."
+L["Summons and dismisses the preferred flying mount."] = "Invoca o retira la montura voladora preferida."
+L["Summons and dismisses the preferred ground mount."] = "Invoca o retira la montura terrestre preferida."
+L["Summons and dismisses the preferred mount capable of transporting a passenger."] = "Invoca o retira la montura para transportar a más de una persona."
+L["Summons and dismisses the preferred mount capable of walking on water."] = "Invoca o retira la montura capaz de caminar sobre el agua."
+L["Summons and dismisses the preferred mount with a vendor."] = "Invoca o retira la montura con vendedor preferida."
+L["Vertical"] = "Vertical"
